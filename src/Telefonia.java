1d1205eaf22b42c52e5d46ec8291f338d16fc796
Felipe Nascimento <93695463+Felipee-Nascimento@users.noreply.github.com>
2023-05-09 18:43:15 -0300

    Update Telefonia.java

--- src/Telefonia.java
+++ src/Telefonia.java
@@ -3,6 +3,15 @@ public class Telefonia {
 
 	private int numPrePagos;
 	private int numPosPagos;
+	private PrePago[] prePagos;
+	private PosPago[] posPagos;
+	
+	
+	public void cadastrarAssinante() {
+		
+		
+		
+	}
 	
 	public static void main(String [] args) {
 		
