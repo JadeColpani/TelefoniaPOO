4ebe9703ab17d38d1a751f1c5d3c7ff78de41c83
Jadoca <jade.colpani@fatec.sp.gov.br>
2023-05-08 22:09:43 -0300

    Create Recarga.java

---
+++ src/Recarga.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+public class Recarga {
+
+	private Date data;
+	private float valor;
+
+	  public Recarga(Date data, float valor){
+
+	    this.data = data;
+	    this.valor = valor;
+	  }
+	  
+	  public Date getData() {
+	    return data;
+	  }
+
+	  public float getValor() {
+	    return valor;
+	  }
+
+	  @Override
+	  public String toString(){
+
+	    return "Data: " + this.data + ", Valor: " + this.valor;
+	  }
+
+}
