4f1c7ebcd8eb0b7a5813a15e3646c0eac06347fe
MariSDiniz <126628807+MariSDiniz@users.noreply.github.com>
2023-05-22 21:16:22 -0300

    Update PosPago.java

--- src/PosPago.java
+++ src/PosPago.java
@@ -4,9 +4,8 @@ public class PosPago extends Assinante{
 	
 	private float assinatura;
 
-	public PosPago(long cpf, String nome, int numero, float assinatura) {
+	public PosPago(long cpf, String nome, int numero) {
 		super(cpf, nome, numero);
-		this.assinatura = assinatura;
 	}
 	
 	public void fazerChamada(GregorianCalendar date, int duracao) {
@@ -14,15 +13,14 @@ public class PosPago extends Assinante{
 		float valorMinuto = 1.04f; 
 		float valorChamada = valorMinuto*duracao; 
 		
-		if (chamadas.length>numChamadas && creditos > valorChamada) {
+		if (chamadas.length>numChamadas) {
 			
 			Chamada chamada = new Chamada(date, duracao);
 			chamadas[numChamadas] = chamada;
 			numChamadas++;
-			creditos = creditos-valorChamada;
+			this.assinatura += valorChamada;
 		}
-		else {
-	
+		else {	
 			
 			System.out.println("Não foi possível realizar a chamada!");
 			
@@ -31,6 +29,23 @@ public class PosPago extends Assinante{
 	
 	public void imprimirFatura(int mes) {
 		
+		float totalFatura=0;
+		
+		System.out.println("CPF: " + getCpf());
+		System.out.println("Nome: " + getNome());
+		System.out.println("Numero: " + getNumero());
+		
+		Chamada[] c = chamadas;
+		for(int i=0;i<chamadas.length;i++) {
+			System.out.println("Data: "+c[i].getData().getTime());
+			System.out.println("Duracao: "+c[i].getDuracao());
+			float custo_ligacao = c[i].getDuracao() * 1.04f; 
+			System.out.println("Custo: R$ "+custo_ligacao);
+			totalFatura = totalFatura + custo_ligacao;
+		}
+
+		System.out.println("Valor da Assinatura: R$ "+this.assinatura);
+		System.out.println("Total da Fatura: R$"+(totalFatura+this.assinatura));
 		
 	}
 
